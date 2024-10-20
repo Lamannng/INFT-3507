42516e97bc3bb23dbf0832262bf835bdcdc626d5
Ləman <122130609+Lamannng@users.noreply.github.com>
2024-10-20 15:35:14 +0400

    Defining the handler for the HTTP requests

--- assignment-1/server.py
+++ assignment-1/server.py
@@ -1,3 +1,13 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import random
 import time
+import json
+from datetime import datetime
+
+class UnreliableHandler(BaseHTTPRequestHandler):
+    # Handling to GET requests
+    def do_GET(self):
+        if self.path == '/getbalance':
+            self.handle_getbalance()
+        elif self.path == '/getlogs':
+            self.handle_getlogs()

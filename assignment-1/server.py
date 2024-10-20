31fbe6bad50242de285aea7b0b7fc0b84de364f3
Ləman <122130609+Lamannng@users.noreply.github.com>
2024-10-20 15:50:49 +0400

    Handling /getbalance route

--- assignment-1/server.py
+++ assignment-1/server.py
@@ -16,3 +16,22 @@ class UnreliableHandler(BaseHTTPRequestHandler):
     def handlegetbalance(self):
         #defining event outcome with the help of probabilities
         event_outcome = random.choices([200, 403, 500, 'timeout'], [0.5, 0.2, 0.1, 0.2])[0]
+        if event_outcome == 'timeout':
+            time.sleep(10) #simulating server timeout
+            return
+        elif event_outcome == 403:
+            self.send_response(403)
+            self.end_headers()
+            self.wfile.write(b"403 Forbidden")
+        elif event_outcome == 500:
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(b"500 Internal Server Error")
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"<html><body><h1>Fake Balance Data</h1></body></html>")
+        #logging the outcome
+        self.log_event(outcome)
+
+        
